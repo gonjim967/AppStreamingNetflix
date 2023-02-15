8f31fa7c77d9a6630461f394a060eb8eaade7ec4
gonjim967 <gonjim967@alu.centroafuera.es>
2023-02-15 12:44:27 +0100

    Un poquito de Login

--- app/src/main/java/com/example/appstreamingnetflix/LoginActivity.kt
+++ app/src/main/java/com/example/appstreamingnetflix/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.example.appstreamingnetflix
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appstreamingnetflix.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    final lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            val vistaMenu = Intent(this, MenuActivity::class.java)
+            startActivity(vistaMenu)
+        }
+
+        binding.button.setOnClickListener{
+            val vistaRegistro = Intent(this, RegistroActivity::class.java)
+            startActivity(vistaRegistro)
+        }
     }
 }
--- app/src/main/java/com/example/appstreamingnetflix/MainActivity.kt
+++ app/src/main/java/com/example/appstreamingnetflix/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.appstreamingnetflix
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +9,7 @@ import com.example.appstreamingnetflix.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     final lateinit var binding: ActivityMainBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,19 +23,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (binding.netflixArray.selectedItem.equals("¿Qué es Netflix?")){
-            binding.textView2.setText("Netflix es un servicio de streaming que ofrece una amplia variedad de series, películas, títulos de anime, documentales y otros contenidos premiados en miles de dispositivos conectados a internet.")
+            binding.textView2.setText("Netflix es un servicio de streaming que ofrece una amplia variedad de series, películas, títulos de anime, documentales y otros contenidos premiados en miles de dispositivos conectados a internet.").toString()
         }
         else if (binding.netflixArray.selectedItem.equals("¿Cuánto cuesta Netflix?")){
-            binding.textView2.setText("Disfruta de Netflix en tu smartphone, tableta, Smart TV, ordenador o dispositivo de streaming, todo por una tarifa mensual fija. Planes desde 5,49 € a 17,99 € al mes. Sin cargos adicionales ni contratos.")
+            binding.textView2.setText("Disfruta de Netflix en tu smartphone, tableta, Smart TV, ordenador o dispositivo de streaming, todo por una tarifa mensual fija. Planes desde 5,49 € a 17,99 € al mes. Sin cargos adicionales ni contratos.").toString()
         }
         else if(binding.netflixArray.selectedItem.equals("¿Dónde puedo ver Netflix?")){
-            binding.textView2.setText("Disfruta donde quieras y cuando quieras. Inicia sesión con tu cuenta de Netflix para disfrutar al instante de todo el contenido de netflix.com desde tu ordenador personal o en cualquier dispositivo conectado a internet que ofrezca la aplicación de Netflix.")
+            binding.textView2.setText("Disfruta donde quieras y cuando quieras. Inicia sesión con tu cuenta de Netflix para disfrutar al instante de todo el contenido de netflix.com desde tu ordenador personal o en cualquier dispositivo conectado a internet que ofrezca la aplicación de Netflix.").toString()
         }
         else if (binding.netflixArray.selectedItem.equals("¿Qué puedo ver en Netflix?")){
-            binding.textView2.setText("Netflix dispone de una amplia biblioteca de originales de Netflix galardonados, títulos de anime, series de TV, documentales, largometrajes y otros contenidos. Ve todo el contenido que quieras, cuando quieras.")
+            binding.textView2.setText("Netflix dispone de una amplia biblioteca de originales de Netflix galardonados, títulos de anime, series de TV, documentales, largometrajes y otros contenidos. Ve todo el contenido que quieras, cuando quieras.").toString()
         }
         else{
-            binding.textView2.setText("La experiencia infantil de Netflix se incluye en la suscripción para que los padres tengan el control mientras los niños disfrutan de series y películas familiares en su propio espacio.")
+            binding.textView2.setText("La experiencia infantil de Netflix se incluye en la suscripción para que los padres tengan el control mientras los niños disfrutan de series y películas familiares en su propio espacio.").toString()
         }
     }
 }
