77f0dd21dd2194ec085b2a0a9f14ff0a8dc0276a
gonza <gonjim967@alu.centroafuera.es>
2023-02-27 15:12:32 +0100

    Video añadido a la mitas, falta con if

--- app/src/main/java/com/example/appstreamingnetflix/MenuActivity.kt
+++ app/src/main/java/com/example/appstreamingnetflix/MenuActivity.kt
@@ -1,11 +1,74 @@
 package com.example.appstreamingnetflix
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appstreamingnetflix.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+    final lateinit var binding: ActivityMenuBinding
+    var peli_seleccionada = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.spider.setOnClickListener{
+            peli_seleccionada="spiderman"
+            val vistaVideo = Intent(this, VideoActivity::class.java)
+            startActivity(vistaVideo)
+        }
+
+        binding.harry.setOnClickListener{
+            peli_seleccionada="harrypotter"
+            val vistaVideo = Intent(this, VideoActivity::class.java)
+            startActivity(vistaVideo)
+        }
+
+        binding.expediente.setOnClickListener{
+            peli_seleccionada="expediente"
+            val vistaVideo = Intent(this, VideoActivity::class.java)
+            startActivity(vistaVideo)
+        }
+
+        binding.breaking.setOnClickListener{
+            peli_seleccionada="breakingbad"
+            val vistaVideo = Intent(this, VideoActivity::class.java)
+            startActivity(vistaVideo)
+        }
+
+        binding.elite.setOnClickListener{
+            peli_seleccionada="elite"
+            val vistaVideo = Intent(this, VideoActivity::class.java)
+            startActivity(vistaVideo)
+        }
+
+        binding.stranger.setOnClickListener{
+            peli_seleccionada="strangerthins"
+            val vistaVideo = Intent(this, VideoActivity::class.java)
+            startActivity(vistaVideo)
+        }
+
+        binding.naruto.setOnClickListener{
+            peli_seleccionada="naruto"
+            val vistaVideo = Intent(this, VideoActivity::class.java)
+            startActivity(vistaVideo)
+        }
+
+        binding.kimetsu.setOnClickListener{
+            peli_seleccionada="kimetsu"
+            val vistaVideo = Intent(this, VideoActivity::class.java)
+            startActivity(vistaVideo)
+        }
+
+        binding.onepiece.setOnClickListener{
+            peli_seleccionada="onepiece"
+            val vistaVideo = Intent(this, VideoActivity::class.java)
+            startActivity(vistaVideo)
+        }
     }
+
+
 }
